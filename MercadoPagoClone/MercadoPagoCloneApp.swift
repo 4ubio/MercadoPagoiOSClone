ef07134e43c0753b2034af05d24daa9224e0d346
Sebastián Rubio Quiroz <sebastian.rubio@iest.edu.mx>
2024-10-08 15:23:33 -0600

    Initial Commit

---
+++ MercadoPagoClone/MercadoPagoCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MercadoPagoCloneApp.swift
+//  MercadoPagoClone
+//
+//  Created by Sebastián Rubio on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MercadoPagoCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MercadoPagoCloneTests/MercadoPagoCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  MercadoPagoCloneTests.swift
+//  MercadoPagoCloneTests
+//
+//  Created by Sebastián Rubio on 08/10/24.
+//
+
+import Testing
+@testable import MercadoPagoClone
+
+struct MercadoPagoCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
